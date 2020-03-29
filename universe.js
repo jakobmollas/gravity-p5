a0305b7646d93b4593c184b046398bdf1b3d6847
Jakob Möllås <jakob.mollas@gmail.com>
2020-03-29 13:38:17 +0200

    Refactor - simplify alorithm using vector functions

--- universe.js
+++ universe.js
@@ -50,25 +50,15 @@ class Universe {
       }
 
       affect(particle) {
-        let mx = particle.position.x;
-        let my = particle.position.y;
+        let nextPosition = createVector(particle.position.x, particle.position.y);
 
         for (let hole of this.blackholes) {
-            let dx = hole.position.x - particle.position.x;
-            let dy = hole.position.y - particle.position.y;
-            //let pull = p5.Vector.sub(hole.position, particle.position);
-            let distance = dx*dx + dy *dy;
-
-            mx += dx*hole.force/distance;
-            my += dy*hole.force/distance;
-            // if (distance != 0.0)
-
-            // pull.div(distance);
-            
-            // steering.add(pull.mult(hole.force));
+            let pull = p5.Vector.sub(hole.position, particle.position);
+            let squareDistance = pull.magSq(pull);
+            let holeInfluence = pull.mult(hole.force).div(squareDistance);
+            nextPosition.add(holeInfluence);
         }
-
-        particle.position.x = mx;
-        particle.position.y = my;
+        
+        particle.position = nextPosition;
     }
 }
