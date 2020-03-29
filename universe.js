7e34d039de3b675c936c6fb84a6256b81940acbe
Jakob Möllås <jakob.mollas@gmail.com>
2020-03-29 11:42:12 +0200

    Refactor

--- universe.js
+++ universe.js
@@ -40,20 +40,3 @@ class Universe {
         }
     }
 }
-
-class BlackHole {
-    constructor() {
-        this.force = random(settings.minGravity, settings.maxGravity);
-        this.position = createVector(random(windowWidth), random(windowHeight));
-    }
-
-    draw() {
-        push();
-        strokeWeight(1);
-        colorMode(RGB);
-        noFill();
-        stroke(255, 50, 50, 255);
-        ellipse(this.position.x, this.position.y, map(this.force, settings.minGravity, settings.maxGravity, 0, 50));
-        pop();
-    }
-}
